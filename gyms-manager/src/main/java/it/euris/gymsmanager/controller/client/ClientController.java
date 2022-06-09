27548c25eb2881ff5b1c391a6ecd75343862394a
gabri <marion.gabriel@volta.ts.it>
2022-06-09 14:57:02 +0200

    Standard_da_cui_partire

--- gyms-manager/src/main/java/it/euris/gymsmanager/controller/client/ClientController.java
+++ gyms-manager/src/main/java/it/euris/gymsmanager/controller/client/ClientController.java
@@ -23,7 +23,7 @@ public class ClientController {
     @Autowired
     ClientServiceImpl clientService;
 
-    @GetMapping(value = "clients")
+    @GetMapping(value = "client")
     public ResponseEntity<List<Client>> getAllGyms() {
         return ResponseEntity.ok(clientService.getAll());
     }
@@ -56,11 +56,10 @@ public class ClientController {
     @DeleteMapping(value ="client/{id}")
     public ResponseEntity deleteById(@PathVariable("id") Long id) {
         clientService.deleteById(id);
-        clientService.getById(id).get();
         return ResponseEntity.ok(HttpStatus.NO_CONTENT);
     }
 
-    @DeleteMapping(value ="api/client/")
+    @DeleteMapping(value ="client/")
     public ResponseEntity deleteAllInBatch() {
         clientService.deleteAllInBatch();
         return ResponseEntity.ok(HttpStatus.NO_CONTENT);
